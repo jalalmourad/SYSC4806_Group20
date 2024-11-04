b2f84676e37fdb376635801c926aea16549a66af
samimnif <samimnif@icloud.com>
2024-11-04 13:19:01 -0500

    initial

---
+++ src/main/java/org/sysc4806/sysc4806_group20/Sysc4806Group20Application.java
@@ -0,0 +1,13 @@
+package org.sysc4806.sysc4806_group20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sysc4806Group20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sysc4806Group20Application.class, args);
+    }
+
+}

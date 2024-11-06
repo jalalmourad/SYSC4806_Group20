31409d1ce409a307ee4efee4f10b61e9787949ba
jalalmourad <114366973+jalalmourad@users.noreply.github.com>
2024-11-06 17:39:29 -0500

    Update StudentModelRepository.java

--- src/main/java/org/sysc4806/sysc4806_group20/StudentModelRepository.java
+++ src/main/java/org/sysc4806/sysc4806_group20/StudentModelRepository.java
@@ -2,7 +2,7 @@ package org.sysc4806.sysc4806_group20;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentModelRepositody extends CrudRepository<StudentModel,Long> {
+public interface StudentModelRepository extends CrudRepository<StudentModel,Long> {
 
     public StudentModel findByName(String firstName, String lastName);
     public StudentModel findStudentModelByStudentID(Long studentID);

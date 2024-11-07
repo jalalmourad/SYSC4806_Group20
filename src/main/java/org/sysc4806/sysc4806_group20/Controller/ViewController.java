a857ada44dd77e88d0bf238927e865185aa4e6ca
samimnif <samimnif@icloud.com>
2024-11-07 00:37:46 -0500

    Added newTopic template

--- src/main/java/org/sysc4806/sysc4806_group20/Controller/ViewController.java
+++ src/main/java/org/sysc4806/sysc4806_group20/Controller/ViewController.java
@@ -3,6 +3,11 @@ package org.sysc4806.sysc4806_group20.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.sysc4806.sysc4806_group20.Model.ProgramRestrictions;
+import org.sysc4806.sysc4806_group20.Model.Status;
+
+import java.util.EnumSet;
+import java.util.ArrayList;
 
 @Controller
 public class ViewController {
@@ -22,6 +27,8 @@ public class ViewController {
      */
     @GetMapping("/newTopic")
     public String newTopic(Model model){
-        return
+        model.addAttribute("status", new ArrayList<>(EnumSet.allOf(Status.class)));
+        model.addAttribute("programs", new ArrayList<>(EnumSet.allOf(ProgramRestrictions.class)));
+        return "newTopic";
     }
 }

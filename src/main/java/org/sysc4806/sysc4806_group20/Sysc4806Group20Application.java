e3a3a7d3e07278bd7d08d78425a19dc185db3574
Aaron Gabor <aarongabor@cmail.carleton.ca>
2024-11-16 01:47:58 -0500

    Created default admin account

--- src/main/java/org/sysc4806/sysc4806_group20/Sysc4806Group20Application.java
+++ src/main/java/org/sysc4806/sysc4806_group20/Sysc4806Group20Application.java
@@ -5,10 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.sysc4806.sysc4806_group20.Model.ProgramRestrictions;
-import org.sysc4806.sysc4806_group20.Model.Status;
-import org.sysc4806.sysc4806_group20.Model.Topic;
+import org.sysc4806.sysc4806_group20.Model.*;
 import org.sysc4806.sysc4806_group20.Repository.TopicRepository;
+import org.sysc4806.sysc4806_group20.Repository.UserAccountRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +17,17 @@ public class Sysc4806Group20Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Sysc4806Group20Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(UserAccountRepository userAccountRepository) {
+        return (args) -> {
+            // Create an admin account if one doesn't exist
+            if (userAccountRepository.findByUsername("admin") == null) {
+                UserAccount admin = new UserAccount("admin", "$2b$12$n58MxR3nDHo70FmtrDDvle2Zal0vxbUUoLdWzNz9cLjD4IxOvnemS", UserRole.ADMIN);
+                userAccountRepository.save(admin);
+            }
+        };
     }
 
 }

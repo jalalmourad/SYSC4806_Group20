3cbdf2f0a9ae30a6f0c61a4fbe5c0145fccab837
Aaron Gabor <aarongabor@cmail.carleton.ca>
2024-12-02 14:14:00 -0500

    Fix to password file implementation.

--- src/main/java/org/sysc4806/sysc4806_group20/Service/PasswordService.java
+++ src/main/java/org/sysc4806/sysc4806_group20/Service/PasswordService.java
@@ -3,15 +3,12 @@ package org.sysc4806.sysc4806_group20.Service;
 import org.springframework.stereotype.Service;
 import org.mindrot.jbcrypt.BCrypt;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
 @Service
 public class PasswordService {
+    private final RestrictedPasswords restrictedPasswords = new RestrictedPasswords();
 
     private String unHashWeb(String hashedPassword) {
         byte[] decodedBytes = Base64.getDecoder().decode(hashedPassword);
@@ -29,25 +26,10 @@ public class PasswordService {
 
     public Boolean checkPassword(String hashedPassword) {
         String password = unHashWeb(hashedPassword);
-        List<String> restrictedPasswords = new ArrayList<>();
-        // Read the restricted passwords from a text file
-        try (BufferedReader reader = new BufferedReader(new FileReader("./restricted_passwords.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                restrictedPasswords.add(line.trim());
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading the restricted passwords file: " + e.getMessage());
-            return true; // Assume false if the file cannot be read
-        }
-
         // Check if the password matches any in the list
-        for (String restrictedPassword : restrictedPasswords) {
-            if (password.equals(restrictedPassword)) {
-                return false; // Invalid password if it matches a restricted password
-            }
+        if(restrictedPasswords.restrictedPasswords.contains(password)) {
+            return false; // Invalid password if it matches a restricted password
         }
-
         return true; // Password is valid
     }
 
@@ -55,5 +37,4 @@ public class PasswordService {
         String decodedHash = unHashWeb(hashedPassword);
         return BCrypt.checkpw(decodedHash, password);
     }
-
 }
